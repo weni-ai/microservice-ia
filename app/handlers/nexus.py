6da7a9083c92e4d82366d470b70f115e47e0d918
Alisson <alissonfilipe937@gmail.com>
2023-12-21 14:38:47 -0300

    fix: NexusRESTClient circular import

---
+++ app/handlers/nexus.py
@@ -0,0 +1,21 @@
+import requests, os
+
+
+class NexusRESTClient:
+    token = os.environ.get("NEXUS_AI_TOKEN")
+    base_url = os.environ.get("NEXUS_AI_URL")
+
+    def __init__(self) -> None:
+        self.headers = {
+            'Authorization': self.token,
+            'Content-Type': "application/json"
+        }
+
+    def index_succedded(self, task_succeded: bool, nexus_task_uuid: str) -> None:
+        endpoint = f'{self.base_url}/api/v1/content-base-file'
+        data = { 
+            "status": int(task_succeded),
+            "task_uuid": nexus_task_uuid,
+        }
+        response = requests.patch(url=endpoint, data=data, headers=self.headers)
+        response.raise_for_status()
